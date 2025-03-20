70953521be523483c04d85af8cbaa5f4c855c4ec
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-20 15:59:29 +0200

    Microservice Project

---
+++ Microservice-devops/Program.cs
@@ -0,0 +1,35 @@
+using Microservice_project.Data;
+using Microservice_project.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<UniversityDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register the services
+builder.Services.AddSingleton<StudentService>();
+builder.Services.AddSingleton<CourseService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
